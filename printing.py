cfcf270261fe5ce94338e654471b476b1e5be512
sunny <propython99@gmail.com>
2020-01-26 21:49:31 -0500

    learn printing different types of data

---
+++ printing.py
@@ -0,0 +1,5 @@
+print("hello") # printing text
+print(1) # printing numbers
+print(True) # printing boolean True or False. Make sure T and F are capitalized.
+print(0b10) # printing binary numbers requires prefixing with 0b or 0B
+print(0x1a) # printing hexadecimal numbers requires prefixing with 0x or 0X
