b6895cf2707e5b3204dc02a85c3f744eb6e621ef
sunny <propython99@gmail.com>
2020-01-26 22:14:23 -0500

    learn about variables

---
+++ var.py
@@ -0,0 +1,8 @@
+# python is a dynamic language
+
+a = 10
+print("Variable \"a\" has a value of :", a, "and its type is :", type(a))
+a = "hello"
+print("Variable \"a\" has a value of :", a, "and its type is :", type(a))
+a = True
+print("Variable \"a\" has a value of :", a, "and its type is :", type(a))
