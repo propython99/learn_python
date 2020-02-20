7e1291ad551a0bf6afc366ac329e9042a986db60
sunny <propython99@gmail.com>
2020-02-19 20:12:21 -0500

    loops

--- second_round/loops.py
+++ second_round/loops.py
@@ -1,1 +1,114 @@
 # two types of loops for and while
+
+# for loop:
+        # scores = [10, 15, 18]
+
+        # for loop works on iterables like lists, strings, dictionaries, sets, tuples, etc.
+        # for i in scores:
+        #     print(f"Congrats! You got a score of {i}")
+
+#while loop:
+
+        # age = 25
+        # # age -= 1 # this is equiavalent to age = age -1
+
+        # while age>18 and age<26:
+        #     print(f"age is {age}")
+        #     age -= 1
+
+        # Iteration 1:
+        # age=25
+
+        # 25>18 and 25<26 - -True
+        # # Output: age is 25
+        # age=24
+
+        # iteration 2: 
+        # age =24
+        # 24>18 and 24<26 -- True
+        # # Output: age is 24
+        # age-> 24-1 -> 23
+
+        # iteration 3: 
+        # age =23
+        # 23>18 and 23<26 -- True
+        # # Output: age is 23
+        # age-> 23-1 -> 22
+
+        # iteration 4: 
+        # age =22
+        # 22>18 and 22<26 -- True
+        # # Output: age is 22
+        # age-> 22-1 -> 21
+
+        # iteration 5: 
+        # age =21
+        # 21>18 and 21<26 -- True
+        # # Output: age is 21
+        # age-> 21-1 -> 20
+
+
+        # while age>18 and age<26:
+        #     print(f"age is {age}")
+        #     age -= 1
+
+        # iteration 6: 
+        # age =20
+        # 20>18 and 20<26 -- True
+        # # Output: age is 20
+        # age-> 20-1 -> 19
+
+        # iteration 7: 
+        # age =19
+        # 19>18 and 19<26 -- True
+        # # Output: age is 19
+        # age-> 19-1 -> 18
+
+        # iteration 8: 
+        # age =18
+        # 18>18 and 18<26 -- False
+        # # End of while loop
+
+        # # while age >= 18:
+        # #     print(f"Your age is {age}. You are eligible to drive!")
+        # #     age -= 1
+        # # else:
+        # #     print(f"Your age is {age}. You are too young to drive!")
+
+# sum of first 5 positive integers -- do it with a while block:
+
+n=5
+
+while n<=5:
+    print(f"num is {n}")
+    
+
+    iteration: 1
+    
+    n=5
+
+    sum=5+(5-1)+(5-2)+(5-3)+(5-4)+(5-5)
+    sum=5+4+3+2+1+0
+    sum=15
+    
+    
+    sum=1
+    sum=n+(sum-1)
+
+    sum=5+0
+
+    sum=5
+    sum=5+4
+
+    sum=9
+    sum=5
+
+
+
+
+
+
+
+
+print("--------end of program-----------")
+
